0307f77f775fffa2ac72690a635f0eb0039427e2
Brandon Shelley <brandon.shelley@delving.co>
2023-02-08 09:16:26 -0800

    Add broken test content

---
+++ src/icons/AircraftPrivateJetMd-broken.tsx
@@ -0,0 +1,53 @@
+const UnCommentForSadness = () => {};
+export default UnCommentForSadness;
+
+// // generated via: https://github.com/brandonscript/mui-react-icons-generator
+// import * as React from "react";
+// import { CSSProperties } from "@mui/styles";
+// import { SvgIcon, SvgIconProps } from "@mui/material";
+// import type { SxProps } from "@mui/system/styleFunctionSx";
+// import { memo } from "react";
+// const SvgAircraftPrivateJetMd = (
+//   props: SvgIconProps & { sx?: SxProps<CSSProperties> }
+// ) => {
+//   const { sx, ...other } = props;
+
+//   const fontSize =
+//     other.fontSize || (sx as any)?.fontSize || other?.style?.fontSize || undefined;
+//   const sizeClassName = fontSize
+//     ? "PacificAviatorSvgIcon-sizeCustom"
+//     : "PacificAviatorSvgIcon-sizeMd";
+//   return (
+//     <SvgIcon
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//       preserveAspectRatio="xMidYMid meet"
+//       viewBox="0 0 30 30"
+//       className={`PacificAviatorSvgIcon-AircraftPrivateJetIcon_md ${sizeClassName}`}
+//       style={{
+//         fill: "none",
+//       }}
+//       sx={{
+//         ...sx,
+//       }}
+//       {...other}
+//     >
+//       <path
+//         d="M10.1 17.212a4.9 4.9 0 1 0 9.8 0 4.9 4.9 0 0 0-9.8 0ZM13.367 15.58h3.266M15 12.313V7.888M11.235 14.078 9.078 11.92M6.29 10.766a1.633 1.633 0 1 0 3.266 0 1.633 1.633 0 0 0-3.266 0ZM18.765 14.078l2.157-2.157M20.444 10.766a1.633 1.633 0 1 0 3.266 0 1.633 1.633 0 0 0-3.266 0Z"
+//         stroke="currentColor"
+//         strokeWidth={1.503}
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//       />
+//       <path
+//         d="M18.163 20.953h6.908a2.178 2.178 0 0 0 2.178-2.177M2.751 18.776a2.177 2.177 0 0 0 2.178 2.177h6.913M12.822 7.888h4.356"
+//         stroke="currentColor"
+//         strokeWidth={1.503}
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//       />
+//     </SvgIcon>
+//   );
+// };
+// const Memo = memo(SvgAircraftPrivateJetMd);
+// export default Memo;
